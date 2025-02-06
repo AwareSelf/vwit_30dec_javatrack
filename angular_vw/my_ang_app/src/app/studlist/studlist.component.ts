1df210f12e241e616a7e36e92d91d97e0f83941b
AwareSelf <namratamarathe81@gmail.com>
2025-02-06 17:51:32 +0530

    angular crud operation with http client calls
    angular crud operation with http client calls

--- angular_vw/my_ang_app/src/app/studlist/studlist.component.ts
+++ angular_vw/my_ang_app/src/app/studlist/studlist.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 import { Student } from '../model/Student';
 import { StudrestclientService } from '../studrestclient.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-studlist',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './studlist.component.html',
   styleUrl: './studlist.component.css'
 })
@@ -13,31 +14,79 @@ export class StudlistComponent {
 
   @Input() max_stud_allowed!:number;
   studarr!:Student[];
-  s1!:Student;
+  updateStud!:Student;
+  flag!:boolean;
+
+
 
 
   constructor(public studrest:StudrestclientService )
   {
      console.log("Student Rest Client Component created..");
-     this.studarr = this.studrest.getAllStudents();
+
+     this.flag = false;
+    // this.updateStud =  new Student();
+     this.studrest.getAllStudents().subscribe((studarr:Student[])=>{
+          this.studarr = studarr;
+          console.log(this.studarr.length);
+
+     })
   }
 
 
   getAllStudents():Student[]
   {
-    this.studarr =  this.studrest.getAllStudents();
+    this.studrest.getAllStudents().subscribe((studarr:Student[])=>{
+      this.studarr = studarr;
+      console.log(this.studarr.length);
+       })
     return this.studarr;
   }
 
   deleteStudent(id:number):void
   {
      console.log('student with studid:'+id+' to be deleted');
+     this.studrest.deleteStudent(id).subscribe((s:string)=>{
+
+      console.log(s);
+      this.studarr = this.studarr.filter(s1=>s1.studid!=id)
+      this.studrest.setStudentArr(this.studarr);
+
+    });
+
+
   }
 
-  updateStudent(s:Student):void
+  editStudent(s:Student):void
   {
     console.log('student to be updated with student id:'+s.studid);
 
+    this.updateStud = new Student(s.studid,s.name,s.totalMarks);
+    this.flag = true;
+  }
+
+  updateStudent():void
+  {
+     this.studrest.updateStudent(this.updateStud).subscribe((s:Student)=>{
+          console.log('updated student:studid:'+s.studid+",name:"+s.name+",totalmarks:"+s.totalMarks);
+
+          this.studarr = this.studarr.map(st=>{
+             if(st.studid===s.studid)
+             {
+                st.name = s.name;
+                st.totalMarks = s.totalMarks;
+                return st;
+             }
+             else
+             {
+               return st;
+            }
+          });
+
+          this.studrest.setStudentArr(this.studarr);
+          this.flag = false;
+
+     });//subscribe ends
   }
 
 
--- angular_vw/my_ang_app/src/app/studrestclient.service.ts
+++ angular_vw/my_ang_app/src/app/studrestclient.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Student } from './model/Student';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable,of } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +10,56 @@ export class StudrestclientService {
 
   studarr:Student[];
   stud!:Student;
+  baseurl:string;
 
-  constructor() {
+  httpoptions =  {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      })
+    }
+
+  constructor(private httpclient:HttpClient) {
     this.studarr= [];
-    this.stud = new Student(1,"Rohit",200);
-    this.studarr.push(this.stud);
-    this.studarr.push(new Student(2,"Sheetal",250));
+    this.baseurl="http://localhost:8080/";
+    }
 
-  }
-
-  getAllStudents():Student[]
+  getAllStudents():Observable<Student[]>
   {
-    return this.studarr.slice(); //return copy of an array and not an original array
+    //am returning the Observable to the caller instead of suscribing it here
+    //as i want it's caller (StudentList Component) to collect the student array in response
+    return  this.httpclient.get<Student[]>(this.baseurl+"all/studs");
   }
 
   addStudent(stud:Student):boolean
   {
-    this.studarr.push(stud);
+    console.log('inside service to add student:name:'+stud.name+",totalMarks:"+stud.totalMarks);
+
+     //push it to backend server
+     let s = {name:stud.name,totalMarks:stud.totalMarks};
+     this.httpclient.post<Student>(this.baseurl+"student",s,this.httpoptions)
+     .subscribe((stu:Student)=>{
+      console.log(stu.studid+","+stu.name+","+stu.totalMarks);
+      this.studarr.push(stu);
+    });
+
     return true;
+  }
+
+  deleteStudent(studid:number):Observable<string>
+  {
+
+    return this.httpclient.delete<string>(this.baseurl+"delete/student/"+studid);
+  }
+
+
+  updateStudent(s:Student):Observable<Student>
+  {
+    return this.httpclient.put<Student>(this.baseurl+"student/update/"+s.studid,s);
+  }
+
+
+  setStudentArr(studarr:Student[]):void
+  {
+    this.studarr = studarr.slice();
   }
 }
