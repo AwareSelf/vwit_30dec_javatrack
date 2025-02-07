28efa212c1b1a1e1b29f1c8e53c27e2400a14fea
AwareSelf <namratamarathe81@gmail.com>
2025-02-07 17:36:50 +0530

    using application properties configuration file based on diff env profiles
    using application properties configuration file based on diff env profiles

--- 7Feb_SpringBootProfileConfigurationPropertiesDemo/usingprofiledemo/src/main/java/nama/springboot/usingprofiledemo/service/RainingDayService.java
+++ 7Feb_SpringBootProfileConfigurationPropertiesDemo/usingprofiledemo/src/main/java/nama/springboot/usingprofiledemo/service/RainingDayService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"raining","test"})
+@Profile({"raining","test","prod"})
 public class RainingDayService implements WeatherService {
 
     @Override
